f59287e569db3e226378380a34e71275654b46d0
utuhiro78 <34818411+utuhiro78@users.noreply.github.com>
2023-01-26 06:10:15 +0900

    Sync

--- src/make.sh
+++ src/make.sh
@@ -7,6 +7,3 @@ ruby convert_alt_cannadic_to_mozcdic.rb
 
 tar cjf mozcdic-ut-alt-cannadic.txt.tar.bz2 mozcdic-ut-alt-cannadic.txt
 mv mozcdic-ut-alt-cannadic.txt* ../
-
-rm -rf ../../mozcdic-ut-alt-cannadic-release/
-rsync -a ../* ../../mozcdic-ut-alt-cannadic-release --exclude=alt-cannadic* --exclude=mozcdic-ut-*.txt
