52d14dac39f5b12f00d13e8a3b2bce137c695888
utuhiro78 <34818411+utuhiro78@users.noreply.github.com>
2024-07-17 04:28:38 +0900

    Convert to Python

---
+++ src/convert_alt_cannadic_to_mozcdic.py
@@ -0,0 +1,109 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# Author: UTUMI Hirosi (utuhiro78 at yahoo dot co dot jp)
+# License: Apache License, Version 2.0
+
+import os
+import re
+import shutil
+import subprocess
+import urllib.request
+from unicodedata import normalize
+
+urllib.request.urlretrieve('https://ftp.iij.ad.jp/pub/osdn.jp/alt-cannadic/50881/alt-cannadic-110208.tar.bz2', 'alt-cannadic-110208.tar.bz2')
+
+if os.path.exists('alt-cannadic-110208'):
+	shutil.rmtree('alt-cannadic-110208')
+
+subprocess.run(['tar', 'xf', 'alt-cannadic-110208.tar.bz2'], check=True)
+
+with open("alt-cannadic-110208/gcanna.ctd", "r", encoding="euc_jp") as f1, \
+open("alt-cannadic-110208/g_fname.ctd", "r", encoding="euc_jp") as f2:
+	lines = f1.read() + f2.read()
+
+lines = lines.splitlines()
+
+# Mozc の一般名詞のID
+url = "https://raw.githubusercontent.com/google/mozc/master/src/data/dictionary_oss/id.def"
+with urllib.request.urlopen(url) as response:
+	id_mozc = response.read().decode()
+
+id_mozc = id_mozc.split(" 名詞,一般,")[0].split("\n")[-1]
+
+dicname = "mozcdic-ut-alt-cannadic.txt"
+l2 = []
+
+for i in range(len(lines)):
+	entry = lines[i].split(" ")
+
+	# あきびん #T35*202 空き瓶 空瓶 #T35*151 空きビン 空ビン
+	yomi = entry[0]
+	yomi = yomi.replace("う゛", "ゔ")
+
+	# 読みがひらがな以外を含む場合はスキップ
+	# 読みが2文字以下の場合はスキップ
+	if yomi != ''.join(re.findall('[ぁ-ゔー]', yomi)) or \
+	len(yomi) < 3:
+		continue
+
+	hinsi = ""
+	cost_anthy = 0
+
+	# 読みを抜いたエントリを作る
+	entry = entry[1:]
+
+	for c in range(len(entry)):
+		# alt-cannadic の品詞とコストを取得
+		# #T35*202 空き瓶 空瓶 #T35*151 空きビン 空ビン
+		if entry[c][0] == "#":
+			s1 = entry[c].split("*")
+			hinsi = s1[0]
+			cost_anthy = int(s1[1])
+			continue
+
+		hyouki = entry[c]
+		hyouki = normalize("NFKC", hyouki)
+
+		# 表記が1文字の場合はスキップ
+		if len(hyouki) < 2:
+			continue
+
+		# alt-cannadic のコストから Mozc 辞書のコストを作る
+		# 「#T35*202 空き瓶 空瓶 #T35*151 空きビン 空ビン」の場合、
+		# 「空き瓶 空瓶 空きビン 空ビン」の順に優先されるようにする。
+		# Mozc 辞書でのコストは 8000 台にする。
+		cost_mozc = (9000 - cost_anthy) + c
+
+		# 収録する品詞を選択
+		if hinsi.startswith("#T3") or \
+		hinsi.startswith("#T0") or \
+		hinsi.startswith("#JN") or \
+		hinsi.startswith("#KK") or \
+		hinsi.startswith("#CN"):
+			s1 = [yomi, hyouki, id_mozc, id_mozc, str(cost_mozc)]
+			l2.append("\t".join(s1))
+
+lines = sorted(l2)
+l2 = []
+
+with open(dicname, "w", encoding="utf-8") as dicfile:
+	dicfile.write("")
+
+for i in range(len(lines)):
+	entry1 = lines[i].split("\t")
+	entry2 = lines[i - 1].split("\t")
+
+	if entry1[:2] == entry2[:2]:
+		continue
+
+	entry1.append(entry1[1])
+	entry1.pop(1)
+
+	l2.append("\t".join(entry1) + "\n")
+
+lines = sorted(l2)
+l2 = []
+
+with open(dicname, "a", encoding="utf-8") as dicfile:
+	dicfile.writelines(lines)
--- src/make.sh
+++ src/make.sh
@@ -3,7 +3,7 @@
 # Author: UTUMI Hirosi (utuhiro78 at yahoo dot co dot jp)
 # License: Apache License, Version 2.0
 
-ruby convert_alt_cannadic_to_mozcdic.rb
+python convert_alt_cannadic_to_mozcdic.py
 
 tar cjf mozcdic-ut-alt-cannadic.txt.tar.bz2 mozcdic-ut-alt-cannadic.txt
 mv mozcdic-ut-alt-cannadic.txt* ../
