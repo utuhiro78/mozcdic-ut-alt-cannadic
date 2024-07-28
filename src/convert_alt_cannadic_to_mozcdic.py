50fee0397b87fe508f9edd45bac56f5290d8ce66
utuhiro78 <34818411+utuhiro78@users.noreply.github.com>
2024-07-29 02:41:35 +0900

    Code style fixes

--- src/convert_alt_cannadic_to_mozcdic.py
+++ src/convert_alt_cannadic_to_mozcdic.py
@@ -29,11 +29,11 @@ id_mozc = id_mozc.split(" 名詞,一般,")[0].split("\n")[-1]
 dict_name = "mozcdic-ut-alt-cannadic.txt"
 l2 = []
 
-for i in range(len(lines)):
-    entry = lines[i].split(" ")
+for line in lines:
+    line = line.split(" ")
 
     # あきびん #T35*202 空き瓶 空瓶 #T35*151 空きビン 空ビン
-    yomi = entry[0]
+    yomi = line[0]
     yomi = yomi.replace("う゛", "ゔ")
 
     # 読みがひらがな以外を含む場合はスキップ
@@ -46,19 +46,18 @@ for i in range(len(lines)):
     cost_anthy = 0
 
     # 読みを抜いたエントリを作る
-    entry = entry[1:]
+    line = line[1:]
 
-    for c in range(len(entry)):
-        # alt-cannadic の品詞とコストを取得
+    for c in range(len(line)):
+        # 「#」で始まるエントリの場合は品詞とコストを取得
         # #T35*202 空き瓶 空瓶 #T35*151 空きビン 空ビン
-        if entry[c][0] == "#":
-            s1 = entry[c].split("*")
-            hinsi = s1[0]
-            cost_anthy = int(s1[1])
+        if line[c][0] == "#":
+            entry = line[c].split("*")
+            hinsi = entry[0]
+            cost_anthy = int(entry[1])
             continue
 
-        hyouki = entry[c]
-        hyouki = normalize("NFKC", hyouki)
+        hyouki = normalize("NFKC", line[c])
 
         # 表記が1文字の場合はスキップ
         if len(hyouki) < 2:
@@ -67,7 +66,7 @@ for i in range(len(lines)):
         # alt-cannadic のコストから Mozc 辞書のコストを作る
         # 「#T35*202 空き瓶 空瓶 #T35*151 空きビン 空ビン」の場合、
         # 「空き瓶 空瓶 空きビン 空ビン」の順に優先されるようにする。
-        # Mozc 辞書でのコストは 8000 台にする。
+        # Mozc 辞書のコストは 8000 台にする。
         cost_mozc = (9000 - cost_anthy) + c
 
         # 収録する品詞を選択
@@ -76,29 +75,26 @@ for i in range(len(lines)):
                 hinsi.startswith("#JN") or \
                 hinsi.startswith("#KK") or \
                 hinsi.startswith("#CN"):
-            s1 = [yomi, hyouki, id_mozc, id_mozc, str(cost_mozc)]
-            l2.append("\t".join(s1))
+            entry = [yomi, hyouki, id_mozc, id_mozc, str(cost_mozc)]
+            l2.append(entry)
 
 lines = sorted(l2)
+l2 = []
+
+for i in range(len(lines)):
+    # 読みと表記が重複する場合はスキップ
+    if lines[i][:2] == lines[i - 1][:2]:
+        continue
+
+    # Mozc 辞書の並びに変更
+    entry = lines[i].copy()
+    entry.append(entry[1])
+    entry.pop(1)
+    l2.append(entry)
+
+lines = l2
 l2 = []
 
 with open(dict_name, "w", encoding="utf-8") as dict_file:
-    dict_file.write("")
-
-for i in range(len(lines)):
-    entry1 = lines[i].split("\t")
-    entry2 = lines[i - 1].split("\t")
-
-    if entry1[:2] == entry2[:2]:
-        continue
-
-    entry1.append(entry1[1])
-    entry1.pop(1)
-
-    l2.append("\t".join(entry1) + "\n")
-
-lines = sorted(l2)
-l2 = []
-
-with open(dict_name, "a", encoding="utf-8") as dict_file:
-    dict_file.writelines(lines)
+    for line in lines:
+        dict_file.write("\t".join(line) + "\n")
